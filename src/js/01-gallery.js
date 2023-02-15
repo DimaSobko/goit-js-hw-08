a8852d306041de880f9ae543bda0d66dea9f3242
DimaSobko <sobko.dima@gmail.com>
2023-02-15 14:56:06 +0200

    первые 2 задания

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsGalleryContainer = galleryMarKup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsGalleryContainer);
+galleryContainer.addEventListener('click', onGalleryContainer)
+
+
+
+function galleryMarKup (galleryItems) {
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    }).join('')
+}
+
+function onGalleryContainer (event) {
+    event.preventDefault();
+    if(event.target.nodeName !=='IMG') {
+        return 
+    } 
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+    scrollZoom: false
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.textarea.addEventListener('input', onTextareaInput)
+
+function onTextareaInput(event) {
+    const value = event.Target.value
+    console.log(value);
+}
 
